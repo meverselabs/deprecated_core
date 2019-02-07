6a40e4bc9f41c8a54943911e1075aa8eee3c197e
Wody <wody@firstchain.co>
2019-02-07 16:37:02 +0900

    remove base

--- kernel/eventhandler.go
+++ kernel/eventhandler.go
@@ -9,26 +9,10 @@ import (
 
 // EventHandler provides callback abilities to the kernel
 type EventHandler interface {
+	// OnCreateContext called when a context creation (error prevent using context)
 	OnCreateContext(kn *Kernel, ctx *data.Context) error
+	// OnProcessBlock called when processing block to the chain (error prevent processing block)
 	OnProcessBlock(kn *Kernel, b *block.Block, s *block.ObserverSigned, ctx *data.Context) error
+	// OnPushTransaction called when pushing a transaction to the transaction pool (error prevent push transaction)
 	OnPushTransaction(kn *Kernel, tx transaction.Transaction, sigs []common.Signature) error
-}
-
-// EventHandlerBase provides empty handler
-type EventHandlerBase struct {
-}
-
-// OnCreateContext called when a context creation (error prevent using context)
-func (eh *EventHandlerBase) OnCreateContext(kn *Kernel, ctx *data.Context) error {
-	return nil
-}
-
-// OnProcessBlock called when processing block to the chain (error prevent processing block)
-func (eh *EventHandlerBase) OnProcessBlock(kn *Kernel, b *block.Block, s *block.ObserverSigned, ctx *data.Context) error {
-	return nil
-}
-
-// OnPushTransaction called when pushing a transaction to the transaction pool (error prevent push transaction)
-func (eh *EventHandlerBase) OnPushTransaction(kn *Kernel, tx transaction.Transaction, sigs []common.Signature) error {
-	return nil
 }
