04fabb1cfc2a018f30dafd1dc59534952b1b81c6
Wody <wody@firstchain.co>
2019-02-13 22:54:03 +0900

    remove unsued config

--- kernel/config.go
+++ kernel/config.go
@@ -4,14 +4,11 @@ import (
 	"time"
 
 	"git.fleta.io/fleta/common"
-	"git.fleta.io/fleta/core/key"
 )
 
 // Config TODO
 type Config struct {
-	ChainCoord        *common.Coordinate
-	ObserverKeys      []string
-	FormulatorAddress common.Address
-	GenTimeThreshold  time.Duration
-	FormulatorKey     key.Key
+	ChainCoord       *common.Coordinate
+	ObserverKeys     []string
+	GenTimeThreshold time.Duration
 }
